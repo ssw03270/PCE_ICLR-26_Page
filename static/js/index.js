694365fc17969381b32acf16ae0cebcd462ba9d7
SeungWon Seo <ttd8591@gmail.com>
2026-02-25 21:14:53 +0900

    init

---
+++ static/js/index.js
@@ -0,0 +1,8 @@
+window.HELP_IMPROVE_VIDEOJS = false;
+
+$(document).ready(function() {
+  $('.navbar-burger').on('click', function() {
+    $('.navbar-burger').toggleClass('is-active');
+    $('.navbar-menu').toggleClass('is-active');
+  });
+});
